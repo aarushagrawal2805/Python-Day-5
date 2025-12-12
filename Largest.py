d0b53844afd91b5d2ef83109d52eb20b78d367e8
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-12 12:55:17 +0530

    Largest Number in list

---
+++ Largest.py
@@ -0,0 +1,12 @@
+# Find the largest item from list
+
+x = [4, 6, 8, 24, 12, 2]
+
+
+def Largestno():
+    Largestno1 = max(x)
+    return Largestno1
+
+
+result = Largestno()
+print("Largest Number is :", res)
