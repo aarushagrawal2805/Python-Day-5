42b87d70edf1c3f9104f9e1e4d22f2c8a63f95a9
aarushagrawal2805 <aarushagrawal62@gmail.com>
2025-12-12 13:39:09 +0530

    Map function

---
+++ Lamdba_map.py
@@ -0,0 +1,9 @@
+#Use a lambda with the map() function to double each element in a list
+
+numbers = [1, 2, 3, 4, 5]
+
+def square():
+    square1=list(map(lambda x:x*x,numbers))
+    return square1
+
+print(square())
